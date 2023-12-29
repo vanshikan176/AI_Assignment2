8952aa74e753ec44ba0ae97467b37dc602651c28
vanshikan176 <155194239+vanshikan176@users.noreply.github.com>
2023-12-30 03:36:10 +0530

    Add files via upload

---
+++ fibo_assign3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=0
+b=1
+n=int(input("enter the number of terms to be printed"))
+print(a)
+print(b)
+d=3
+while d<=n:
+    c=a+b
+    print(c)
+    a=b
+    b=c
+    d=d+1
+
+
+# In[ ]:
+
+
+
+
